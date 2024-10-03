bd75792be063032afa416430035e7ac8d669e75a
Jan Kühnlein <jan.kuehnlein@kultify.com>
2024-10-03 20:06:50 +0200

    chore: update example

--- src/example/example1.ts
+++ src/example/example1.ts
@@ -10,13 +10,21 @@ export async function main() {
 		'0.0.1',
 	);
 	kmicroService1.addEndpoint('hello', async (context, data) => {
-		console.log('handle > hello', JSON.stringify(context.context), data);
-		return {foo: 'bar'};
+		console.log(
+			'handle > hello',
+			JSON.stringify(context.context),
+			JSON.parse(Buffer.from(data).toString()),
+		);
+		return Buffer.from(JSON.stringify({foo: 'bar'}));
 	});
 
 	kmicroService1.addEndpoint('get_data', async (context, data) => {
-		console.log('handle > get_data', JSON.stringify(context.context), data);
-		return {response: [data, data]};
+		console.log(
+			'handle > get_data',
+			JSON.stringify(context.context),
+			JSON.parse(Buffer.from(data).toString()),
+		);
+		return Buffer.from(JSON.stringify({response: [data, data]}));
 	});
 
 	// setup service 2
@@ -27,9 +35,15 @@ export async function main() {
 	);
 	kmicroService2.addEndpoint('gather', async (context, data) => {
 		console.log('handle > gather', data);
-		const result = await context.call('service1.get_data', {foo: 'Bar2'});
-		console.log('got data from service1', result);
-		return {data: result};
+		const result = await context.call(
+			'service1.get_data',
+			Buffer.from(JSON.stringify({foo: 'Bar2'})),
+		);
+		console.log(
+			'got data from service1',
+			JSON.parse(Buffer.from(result).toString()),
+		);
+		return Buffer.from(JSON.stringify({data: result}));
 	});
 	console.log('Server 2 listening');
 
@@ -37,10 +51,16 @@ export async function main() {
 	const caller = await init(nats, 'example1', '1.0.0');
 	console.log('call service1.hello');
 
-	const response = await caller.call('service1.hello', {hello: 'world call'});
+	const response = await caller.call(
+		'service1.hello',
+		Buffer.from(JSON.stringify({hello: 'world call'})),
+	);
 	console.log('Response', response);
 
-	const response2 = await caller.call('service2.gather', {});
+	const response2 = await caller.call(
+		'service2.gather',
+		Buffer.from(JSON.stringify({})),
+	);
 	console.log('Response', response2);
 
 	await kmicroService1.stop();
